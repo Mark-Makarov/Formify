128ed3f069b9b8b3a1e128d2941966b7b8bf17c2
Mark-Makarov <mark.makararov@yandex.ru>
2024-04-11 21:30:31 +0300

    update packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { ruRU } from "@clerk/localizations";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/toaster";
