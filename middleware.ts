fc037cba73e07e0011e5715c89e2c7a9fc3ad287
Mark-Makarov <mark.makararov@yandex.ru>
2024-04-11 17:06:09 +0300

    edit configs

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ['/', '/api/webhook/clerk', '/sign-in', '/sign-up'], ignoredRoutes: ['/api/webhook/clerk'], debug: true});
+export default authMiddleware({ publicRoutes: ['/', '/api/webhook/clerk', '/sign-in', '/sign-up'], ignoredRoutes: ['/api/webhook/clerk']});
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
