aa7dd2c739c40fa0f5e708e31fdc8e296332a0f4
Mark-Makarov <mark.makararov@yandex.ru>
2024-04-14 01:47:19 +0300

    change config

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,10 @@
-export function middleware() {}
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: ["/"],
+  ignoredRoutes: ["/forms", "/builder", "/submit"],
+  authorizedParties: ["http://localhost:3000", "http://localhost:3458", "https://formify.astroflare.online"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
